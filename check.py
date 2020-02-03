e29930b017338b627c7e1a92b1edce0c6dacc49d
hpmario <ivan.dovger@mail.ru>
2020-02-03 12:03:52 +0300

    ыавммаы

--- check.py
+++ check.py
@@ -14,7 +14,7 @@ def b():
             duration.append(0)
     some_series.insert(3, 'new column', duration)
     print(some_series.loc[some_series['new column'].astype(float).idxmax()][['ObjectName', 'new column']])
- #   print("Unknown  ", count_of_nan)
+    print("Unknown  ", count_of_nan)
     dict = {'Object': [some_series.loc[some_series['new column'].astype(float).idxmax()]['ObjectName'], 'NaN date'], 'Duration': [ some_series['new column'].max(), count_of_nan]}
     output = pd.DataFrame(dict)
-    #output.to_csv('general-stats.csv')
+    output.to_csv('general-stats.csv')
